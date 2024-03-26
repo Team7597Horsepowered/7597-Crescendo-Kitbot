58f46261b6ce169208cc85cc15b1fcdcf303bdc0
SteelingCow437 <davidandmatt2@yahoo.com>
2024-03-26 13:40:18 -0400

    A bit closer every day

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,10 +10,16 @@ import frc.robot.commands.ClimbDown;
 import frc.robot.commands.ClimbExtender;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.commands.ShootCommand;
+import frc.robot.commands.SwerveCommand;
 import frc.robot.commands.TankCommand;
 import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TankSubsystem;
+import frc.robot.swerve.SwerveSubsystem;
+
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ScheduleCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -30,12 +36,13 @@ public class RobotContainer {
   private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
   private final TankSubsystem m_tankSubsystem = new TankSubsystem();
   private final ClimbSubsystem m_ClimbSubsystem = new ClimbSubsystem();
-
+  private final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/neo"));
   private final IntakeCommand m_IntakeCommand = new IntakeCommand(m_ShooterSubsystem);
   private final ShootCommand m_ShootCommand = new ShootCommand(m_ShooterSubsystem);
   private final TankCommand m_tankCommand = new TankCommand(m_tankSubsystem);
   private final ClimbExtender m_ClimbExtender = new ClimbExtender(m_ClimbSubsystem);
   private final ClimbDown m_ClimbDown = new ClimbDown(m_ClimbSubsystem);
+  private final SwerveCommand m_SwerveCommand = new SwerveCommand(m_SwerveSubsystem, null, null, null);
   private final AutonomousCommand command = new AutonomousCommand();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
